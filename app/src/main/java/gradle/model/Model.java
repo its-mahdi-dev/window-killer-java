a5ac323165ca1ee5ce09ee5a16b02ce04d65a2d3
mahdi <mhdrslzdh1383@gmail.com>
2024-04-08 15:44:31 +0330

    add model class

--- app/src/main/java/gradle/model/Model.java
+++ app/src/main/java/gradle/model/Model.java
@@ -1,5 +1,30 @@
 package gradle.model;
 
-public class Model {
-    
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Model {
+    private String Id;
+    public static final List<Model> items = new ArrayList<>();
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String Id) {
+        this.Id = Id;
+    }
+
+    public Model findModel(String Id) {
+        for (Model item : items) {
+            if (item.getId().equals(Id)) {
+                return item;
+            }
+        }
+        return null;
+    }
+
+    public static void addItem(Model item) {
+        items.add(item);
+    }
 }

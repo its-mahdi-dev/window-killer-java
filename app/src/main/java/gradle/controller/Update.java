6ceb541ed56e67f6bfa26c3b52f4bf2ba8e897ec
mahdi <mhdrslzdh1383@gmail.com>
2024-04-11 20:00:43 +0330

    check removed shot

--- app/src/main/java/gradle/controller/Update.java
+++ app/src/main/java/gradle/controller/Update.java
@@ -9,6 +9,7 @@ import gradle.view.charecretsView.EpsilonView;
 import gradle.view.charecretsView.ShotView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Update {
 
@@ -35,10 +36,16 @@ public class Update {
     }
 
     public void updateModel() {
-
         for (int i = 0; i < ShotModel.items.size(); i++) {
             ShotModel shotModel = (ShotModel) ShotModel.items.get(i);
             shotModel.move();
+            if (shotModel.anchor.getX() + Constants.SHOT_DIAMETER > Constants.PANEL_SIZE.getWidth()
+                    || shotModel.anchor.getX() < 0 ||
+                    shotModel.anchor.getY() + Constants.SHOT_DIAMETER > Constants.PANEL_SIZE.getHeight()
+                    || shotModel.anchor.getY() < 0) {
+                ShotModel.items.remove(i);
+                ShotView.items.removeIf(shot -> shot.getId() == shotModel.getId());
+            }
         }
     }
 }

b71cd1f4be5acd3b41f1c9010440326d631ff1c1
mahdi <mhdrslzdh1383@gmail.com>
2024-05-02 15:54:19 +0330

    add health skill

--- app/src/main/java/gradle/controller/SkillTreeController.java
+++ app/src/main/java/gradle/controller/SkillTreeController.java
@@ -2,6 +2,9 @@ package gradle.controller;
 
 import java.util.*;
 import java.awt.event.*;
+import javax.swing.Timer;
+
+import gradle.model.EpsilonModel;
 
 public class SkillTreeController {
     public static final Map<String, Long> skillsTime = new HashMap<>();
@@ -19,12 +22,22 @@ public class SkillTreeController {
 
     public static final long MIN_SKILL_TIME = 5 * 60000;
 
+    static Timer timer = new Timer(1000, new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (skills.get("aceso"))
+                EpsilonModel.items.get(0).HP = Math.min(100, EpsilonModel.items.get(0).HP + 1);
+        }
+    });;
+
     public static void keyControl(KeyEvent e) {
         switch (e.getKeyCode()) {
             case KeyEvent.VK_F:
                 setAres();
                 break;
-
+            case KeyEvent.VK_H:
+                setAceso();
+                break;
             default:
                 break;
         }
@@ -37,12 +50,23 @@ public class SkillTreeController {
         }
     }
 
+    private static void setAceso() {
+        if (!skills.get("aceso")) {
+            skillsTime.replace("aceso", System.currentTimeMillis());
+            skills.replace("aceso", true);
+            timer.start();
+        }
+    }
+
     public static void checkSkillsTime() {
         for (Map.Entry<String, Long> entry : skillsTime.entrySet()) {
             if (System.currentTimeMillis() - entry.getValue() > MIN_SKILL_TIME) {
                 skills.replace(entry.getKey(), false);
             }
         }
+        if (!skills.get("aceso"))
+            timer.stop();
+
     }
 
 }

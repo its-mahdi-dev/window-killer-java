03fd9e7c947a9fb173ef4f8347a27febf20c77f4
mahdi <mhdrslzdh1383@gmail.com>
2024-05-03 12:50:57 +0330

    stack the ares and aceso

--- app/src/main/java/gradle/controller/SkillTreeController.java
+++ app/src/main/java/gradle/controller/SkillTreeController.java
@@ -8,27 +8,17 @@ import gradle.model.EpsilonModel;
 
 public class SkillTreeController {
     public static final Map<String, Long> skillsTime = new HashMap<>();
-    public static final Map<String, Boolean> skills = new HashMap<>();
-
+    public static int enemy_hp_decrease;
+    public static int epsilon_hp_increase;
     static {
         skillsTime.put("ares", System.currentTimeMillis() - 6 * 60000);
         skillsTime.put("aceso", System.currentTimeMillis() - 6 * 60000);
         skillsTime.put("proteus", System.currentTimeMillis() - 6 * 60000);
-
-        skills.put("ares", false);
-        skills.put("aceso", false);
-        skills.put("proteus", false);
     }
 
     public static final long MIN_SKILL_TIME = 5 * 60000;
 
-    static Timer timer = new Timer(1000, new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            if (skills.get("aceso"))
-                EpsilonModel.items.get(0).HP = Math.min(100, EpsilonModel.items.get(0).HP + 1);
-        }
-    });;
+    static Timer timer;
 
     public static void keyControl(KeyEvent e) {
         switch (e.getKeyCode()) {
@@ -46,26 +36,27 @@ public class SkillTreeController {
     private static void setAres() {
         if (System.currentTimeMillis() - skillsTime.get("ares") > MIN_SKILL_TIME) {
             skillsTime.put("ares", System.currentTimeMillis());
-            skills.replace("ares", true);
+            enemy_hp_decrease += 2;
         }
     }
 
     private static void setAceso() {
-        if (!skills.get("aceso")) {
+        if (System.currentTimeMillis() - skillsTime.get("aceso") > MIN_SKILL_TIME) {
             skillsTime.replace("aceso", System.currentTimeMillis());
-            skills.replace("aceso", true);
+            epsilon_hp_increase += 1;
+            timer = new Timer(1000, new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (System.currentTimeMillis() - skillsTime.get("aceso") < MIN_SKILL_TIME)
+                        EpsilonModel.items.get(0).HP = Math.min(100,
+                                EpsilonModel.items.get(0).HP + epsilon_hp_increase);
+                }
+            });
             timer.start();
         }
     }
 
     public static void checkSkillsTime() {
-        for (Map.Entry<String, Long> entry : skillsTime.entrySet()) {
-            if (System.currentTimeMillis() - entry.getValue() > MIN_SKILL_TIME) {
-                skills.replace(entry.getKey(), false);
-            }
-        }
-        if (!skills.get("aceso"))
-            timer.stop();
 
     }
 
